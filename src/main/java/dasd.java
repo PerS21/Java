deea46acbd036239b160d10f9b39729f581f150d
PerS21 <perepel651@mail.ru>
2019-09-07 17:40:42 +0300

    Initial commit

---
+++ src/main/java/dasd.java
@@ -0,0 +1,80 @@
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class HttpURLConnectionExample {
+
+    private Object JsonNode;
+    private Object JSONObject;
+
+    String getLat = "55.733661";
+    String getLon = "37.857681";
+    String getLang = "«ru_RU»";
+    String getLimit = "2";
+    String getHours = "true";
+    String getExtra = "true";
+
+
+
+
+    public static void main(String[] args) throws Exception {
+
+        HttpURLConnectionExample http = new HttpURLConnectionExample();
+
+        System.out.println("Testing 1 - Send Http GET request");
+        http.sendGet();
+    }
+
+
+
+
+    private void sendGet() throws Exception {
+
+
+ //       String url = "https://api.weather.yandex.ru/v1/forecast/?lat=55.733661&lon=37.857681&lang=«ru_RU»&limit=2&hours=true&extra=true";
+        String url = "https://api.weather.yandex.ru/v1/forecast/?lat="+ getLat +"&lon=" + getLon + "&lang=" + getLang + "&limit=" + getLimit + "&hours=" + getHours + "&extra=" + getExtra;
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("X-Yandex-API-Key", "cf96abee-1083-4c09-9738-67abd189eab1");
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+//        JSONObject jsonObj = new JSONObject(response);
+//
+//        System.out.println(jsonObj);
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        JSONObject jsonObject = mapper.readValue(String.valueOf(response), JSONObject.class);
+//
+////        JSONObject asd = new JSONObject(jsonObject);
+//
+//        System.out.println(jsonObject);
+    }
+}
